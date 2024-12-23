4780ebc2e012d8621badd8dadf0130ddd98eeae9
Romuald Dufour <54169008+rdu70@users.noreply.github.com>
2024-12-23 21:29:01 +0100

    Add files via upload

--- include/cred_sample.h
+++ include/cred_sample.h
@@ -1,1 +1,13 @@
+#ifndef _CRED_H
+#define _CRED_H
 
+// Wifi credential
+#define WIFI_SSID "ChangeMe_SSID"
+#define WIFI_PSK "ChangeMe_PSK"
+
+// MQTT Credential
+#define MQTT_IP "ChangeMe_MQTT_Server_IP"
+#define MQTT_USER "ChangeMe_MQTT_Username"
+#define MQTT_PASS "ChangeMe_MQTT_Password"
+
+#endif  /* _CRED_H */
